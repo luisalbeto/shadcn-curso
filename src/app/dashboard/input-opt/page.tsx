b5c9fff75cb2057f1a719d5e3598608bdf88cba5
¨luisalbeto¨ <luis-alberto11@hotmail.com>
2024-08-07 16:15:53 -0400

    Feat: menubar & input opt components

---
+++ src/app/dashboard/input-opt/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import { useState } from "react"
+
+
+export default function Page() {
+
+  const [value, setValue] = useState("")
+  return (
+    <div className="flex flex-col justify-center items-center h-[250px]">
+      <InputOTP 
+      maxLength={4}
+      value={value}
+      onChange={(value) => setValue(value)}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+        </InputOTPGroup>
+      </InputOTP>
+
+      <div className="mt-2">
+        {value}
+      </div>
+
+    </div>
+  )
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,6 +16,10 @@ const links = [
   { name: "card", href: "card" },
   { name: "carousel", href: "carousel" },
   { name: "checkbox", href: "checkbox" },
+  { name: "menuBar", href: "menu-bar" },
+  { name: "input-opt", href: "input-opt" },
+
+
 
 ].sort((a, b) => a.name.localeCompare(b.name))
 
@@ -156,7 +160,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
@@ -268,7 +272,7 @@ export default function DashboardLayout({
               </a>
             </div>
           </footer>
-          <p className="text-center text-sm text-gray-500 my-10">
+          <p className="text-center text-sm text-gray-500 my-10 print:hidden">
             &copy; {new Date().getFullYear()}{" "}
             <a href="#" className="hover:underline" target="_blank">
               Luis Ramirez
